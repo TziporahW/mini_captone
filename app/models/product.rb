b368e0430bcc9aeb60bae7188f9aa4f3e1f5845a
Tziporah <tziporawerzberger@gmail.com>
2020-11-12 14:09:15 -0500

    adds categories

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  has_many :product_categories
+  has_many :categories, through: :product_categories
   belongs_to :supplier
   has_many :images
   has_many :orders
